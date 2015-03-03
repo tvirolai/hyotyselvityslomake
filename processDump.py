ecb4bf9120f9f98b3e9bc1cd6ae44e3f6d7fab7d
Tuomo Virolainen <tvirolai@kuvailu.lib.helsinki.fi>
2015-03-03 12:15:23 +0200

    Update

--- processDump.py
+++ processDump.py
@@ -9,14 +9,40 @@ def read(dumppi):
 	return lista
 
 def tulosta(lista):
+	tilasto = {}
+	total = 0
 	for line in lista:
-		try:
-			aika = calculate(int(line[4]))
-		except:
-			aika = "(ei oo)"
+		aika = calculate(int(line[4]))
 		print "ID: %s \nAINEISTO: %s \nTAPAUS: %s \nOSAKOHTEET: %s \nAIKA: %s \nKOMMENTTI: %s \nAIKALEIMA: %s" \
-		% (
-			line[0], line[1], line[2], line[3], aika, line[5], line[6])
+		% (line[0], line[1], line[2], line[3], aika, line[5], line[6])
+		print "================"
+
+		if (line[3] != ""):
+			tapaus = line[1] + " : " + line[2] + " : " + line[3]
+		else:
+			tapaus = line[1] + " : " + line[2] + " : (ei osakohteita)"
+
+		sekunnit = int(line[4])
+
+		total += 1
+
+		if not tapaus in tilasto:
+			tilasto[tapaus] = [1, sekunnit, sekunnit, sekunnit, sekunnit] # määrä, keskiarvo, minimi, maksimi, summa
+		else:
+			tilasto[tapaus][0] += 1
+			tilasto[tapaus][4] += sekunnit
+			tilasto[tapaus][1] = tilasto[tapaus][4] / tilasto[tapaus][0]
+			if (sekunnit < tilasto[tapaus][2]):
+				tilasto[tapaus][2] = sekunnit
+			if (sekunnit > tilasto[tapaus][3]):
+				tilasto[tapaus][3] = sekunnit
+
+	for key in sorted(tilasto):
+		print key
+		print "TAPAUKSIA: " + str(tilasto[key][0]) + " (" + str(round(float(tilasto[key][0]) / total * 100, 1)) + " %)"
+		print "KESKIARVO: " + str(calculate(tilasto[key][1]))
+		print "MINIMI: " + str(calculate(tilasto[key][2]))
+		print "MAKSIMI: " + str(calculate(tilasto[key][3]))
 		print "================"
 
 def calculate(rivi):
